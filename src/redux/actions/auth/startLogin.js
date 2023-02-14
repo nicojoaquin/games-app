9a3b3ba29ebda5e0637e9d9b9dc065972e9c9ba7
unknown <nicolas.joaquin@greelow.com>
2023-02-13 23:22:44 -0300

    signup form error handle

--- src/redux/actions/auth/startLogin.js
+++ src/redux/actions/auth/startLogin.js
@@ -1,35 +1,41 @@
-import { fetcnNoToken } from '../../../helpers/fetch';
-import { setCheckingLogin, setLogin } from '../../reducers/authReducer';
-import { addAlert } from '../../../components/assets/alert';
+import { fetcnNoToken } from "../../../helpers/fetch";
+import { setCheckingLogin, setLogin } from "../../reducers/authReducer";
+import { addAlert } from "../../../components/assets/alert";
 
 export const startLogin = (email, password, endpoint) => async (dispatch) => {
- 
   const url = `${process.env.REACT_APP_API_URL}/${endpoint}`;
 
   dispatch(setCheckingLogin(true));
 
   try {
-
-    const res = await fetcnNoToken(url, {
-      email,
-      password,
-    }, 'post')
+    const res = await fetcnNoToken(
+      url,
+      {
+        email,
+        password,
+      },
+      "post"
+    );
 
     const data = await res.data;
 
-    localStorage.setItem('token', data.token);
-    localStorage.setItem('token-init-date', new Date().getTime());
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("token-init-date", new Date().getTime());
 
-    dispatch(setLogin({
-      uid: data.uid,
-      name: data.name
-      }));
-    
-    
+    dispatch(
+      setLogin({
+        uid: data.uid,
+        name: data.name,
+      })
+    );
   } catch (err) {
-      addAlert(err?.response?.data?.msg, "error")
-  } finally{
-      dispatch(setCheckingLogin(false));
+    addAlert(
+      err.response.data.errors
+        ? Object.values(err.response.data.errors)[0].msg
+        : err.response.data.msg,
+      "error"
+    );
+  } finally {
+    dispatch(setCheckingLogin(false));
   }
-
-}
+};
--- src/redux/actions/auth/startRegister.js
+++ src/redux/actions/auth/startRegister.js
@@ -1,36 +1,41 @@
-import { fetcnNoToken } from '../../../helpers/fetch';
-import {  setCheckingRegister, setLogin } from '../../reducers/authReducer';
-import { addAlert } from '../../../components/assets/alert';
+import { fetcnNoToken } from "../../../helpers/fetch";
+import { setCheckingRegister, setLogin } from "../../reducers/authReducer";
+import { addAlert } from "../../../components/assets/alert";
 
-export const startRegister = (email, password, name, endpoint) => async (dispatch) => {
+export const startRegister =
+  (email, password, name, endpoint) => async (dispatch) => {
+    const url = `${process.env.REACT_APP_API_URL}/${endpoint}`;
 
-  const url = `${process.env.REACT_APP_API_URL}/${endpoint}`;
+    dispatch(setCheckingRegister(true));
 
-  dispatch(setCheckingRegister(true));
+    try {
+      const { data } = await fetcnNoToken(
+        url,
+        {
+          email,
+          password,
+          name,
+        },
+        "post"
+      );
 
-  try {
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
 
-    const res = await fetcnNoToken(url, {
-      email,
-      password,
-      name
-    }, 'post')
-    
-    const data = await res.data;
-    
-    localStorage.setItem('token', data.token);
-    localStorage.setItem('token-init-date', new Date().getTime());
-  
-    dispatch(setLogin({
-      uid: data.uid,
-      name: data.name
-     }));
-
-    
-  } catch (err) {
-      addAlert(err.response.data.msg, "error")
-  } finally {
-      dispatch(setCheckingRegister(true));
-  }
-
-}
+      dispatch(
+        setLogin({
+          uid: data.uid,
+          name: data.name,
+        })
+      );
+    } catch (err) {
+      addAlert(
+        err.response.data.errors
+          ? Object.values(err.response.data.errors)[0].msg
+          : err.response.data.msg,
+        "error"
+      );
+    } finally {
+      dispatch(setCheckingRegister(false));
+    }
+  };
